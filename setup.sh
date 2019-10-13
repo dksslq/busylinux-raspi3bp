5f70d97d5135be4ba914b602402da9b9c1ca9fe7
dksslq <dksslq@gmail.com>
2019-10-13 06:14:32 +0000

    优化

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -e
 
 echo "警告! 进行安装操作将会清除目标安装目录中的旧文件!"
 echo
 
-BOOT="$PWD/boot/*"
+BOOT="$PWD/boot"
 
 ROOTBASIC="$PWD/root-basic.cpio"
 LIBS="$PWD/software/basic/lib"
@@ -21,22 +22,21 @@ while [ -z "$SD_ROOTDIR" -o ! -d "$SD_ROOTDIR" ];do
 	printf "输入SD卡root分区挂载点: ";read SD_ROOTDIR
 done
 
-set -e
-
 # clean old files
 find "$SD_BOOTDIR" -maxdepth 1 -! -path "$SD_BOOTDIR" -exec rm -rf {} \;
 find "$SD_ROOTDIR" -maxdepth 1 -! -path "$SD_ROOTDIR" -exec rm -rf {} \;
 sync
 
-cp -rfa $BOOT $SD_BOOTDIR
+cp -rfa "$BOOT/." $SD_BOOTDIR
+#find "$BOOT" -maxdepth 1 -! -path "$BOOT" -exec cp -rfa {} "$SD_BOOTDIR" \;
 
 #search and install *.tar.xz from $1 depth 1
 dist_install_xzs(){
-	find $1 -maxdepth 1 -type f -name "*.tar.xz" -exec tar xJvf {} -C $SD_ROOTDIR \;
+	find $1 -maxdepth 1 -type f -name "*.tar.xz" -exec tar -k -xJvf {} -C $SD_ROOTDIR \;
 	wait
 }
 dist_install_xz(){
-	tar xJvf $1 -C $SD_ROOTDIR
+	tar -k -xJvf $1 -C $SD_ROOTDIR
 	wait
 }
 
